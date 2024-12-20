0280241c4733baf7d7110addfd26b0fc182fe54e
Evgenii Korobkin <e.korobkin@astondevs.ru>
2024-12-20 12:27:44 +0300

    feat: add default layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import { Header } from "@/components/shared/header";
 
 export default function Home() {
   return (
-    <h1>
-      <Button variant="outline">Корзина</Button>
-    </h1>
+    <div>
+      Главная
+    </div>
   );
 }
