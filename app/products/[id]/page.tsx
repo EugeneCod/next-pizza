310f362b11e315f6cdd4b8b3b7a782f6636e4ccd
Evgenii Korobkin <e.korobkin@astondevs.ru>
2025-01-07 13:48:02 +0300

    feat: add initial product page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,29 @@
-import { NextPage } from 'next';
+import { Container, GroupVariants, ProductImage, Title } from '@/components/shared';
+import { prisma } from '@/prisma/prisma-client';
+import { notFound } from 'next/navigation';
 
-export default function ProductPage({ params: { id } }: PageWithParams) {
-  return <div>Product {id}</div>;
+export default async function ProductPage({ params: { id } }: PageWithParams) {
+  const product = await prisma.product.findFirst({ where: { id: Number(id) } });
+
+  if (!product) return notFound();
+
+  return (
+    <Container className="flex flex-col my-10">
+      <div className="flex flex-1">
+        <ProductImage imageUrl={product.imageUrl} size={40} />
+        <div className="w-[490px] bg-[#FCFCFC] p-7 rounded-2xl">
+          <Title text={product.name} size="md" className="font-extrabold mb-1" />
+          <p className="text-gray-400">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <GroupVariants
+            selectedValue="1"
+            items={[
+              { name: 'Маленькая', value: '1' },
+              { name: 'Средняя', value: '2' },
+              { name: 'Большая', value: '3' },
+            ]}
+          />
+        </div>
+      </div>
+    </Container>
+  );
 }
