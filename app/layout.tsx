964576460579d8a1728adaa1149cfd28ab18f1dd
EugeneKGen <eugene_korobkin@mail.ru>
2025-02-02 17:50:17 +0300

    refactor: change the path to logos

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
-        <link data-rh="true" rel="icon" href="/logo.png" />
+        <link data-rh="true" rel="icon" href="/assets/images/logos/react-pizza.png" />
       </head>
       <body className={nunito.variable}>
         <Providers>{children}</Providers>
